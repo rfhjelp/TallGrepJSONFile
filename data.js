484b11c215682328987d7e316e78f915116c8c12
rfhjelp <andblugje@gmail.com>
2025-02-23 17:15:52 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -6,108 +6,133 @@ const data = [
                 navn: "De fire regneartene: addisjon og subtraksjon",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "https://udeoslokommuneno-my.sharepoint.com/:b:/g/personal/anbla010_osloskolen_no/EdWqbPUCQVJBoVgPyFAxZV8B9t4Vo8XsDxySdsaQ_DSRgw?e=OmMmCj",
                 keywords: ["pluss", "minus", "subtraksjon", "addisjon", "regning", "tall"]
             },
             {
                 navn: "De fire regneartene: multiplikasjon og divisjon",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "https://udeoslokommuneno-my.sharepoint.com/:b:/g/personal/anbla010_osloskolen_no/EbB_DTuZGZlKufE8Ts-EVWQB99392eV_WRgHCeCsNvTmhQ?e=5pZHfn",
                 keywords: ["ganging", "deling", "multiplikasjon", "divisjon", "regning", "tall"]
+            },
+            {
+                navn: "Brøk: desimaltall og prosent",
+                lettVideo: "",
+                dypereVideo: "",
+                oppgaver: "https://udeoslokommuneno-my.sharepoint.com/:b:/g/personal/anbla010_osloskolen_no/EVClhpubCChGmFjHhPjffmYB6ARWsPifoDsFUoG3ZtgTvA?e=ffxS3h",
+                keywords: ["desimaltall", "brøk", "prosent"]
             },
             {
                 navn: "Brøk: addisjon og subtraksjon",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "https://udeoslokommuneno-my.sharepoint.com/:b:/g/personal/anbla010_osloskolen_no/EWTTc6A9kFhMg2wkoA5F4xwBTLES8hyMDkpUO6B2ffUi8g?e=9cZ90o",
                 keywords: ["pluss", "minus", "subtraksjon", "addisjon", "brøk"]
             },
             {
                 navn: "Brøk: multiplikasjon og divisjon",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "https://udeoslokommuneno-my.sharepoint.com/:b:/g/personal/anbla010_osloskolen_no/EQwAm37G9rNOqMBjM4GyBSgBtKyFjervGqE8mYNsSlup3Q?e=3ehj1o",
                 keywords: ["ganging", "deling", "multiplikasjon", "divisjon", "brøk"]
             },
             {
                 navn: "Tall regning: potenser og paranteser",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["tall", "regning", "potens", "eksponent", "grunntall", "parantes"]
             },
             {
                 navn: "Tall regning: kvadratrot og kvadrattall",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["tall", "regning", "kvadratrot", "kvadrattall"]
             },
             {
                 navn: "Tall regning: PEMDAS",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["tall", "regning", "rekkefølgen", "pemdas", "regne"]
             },
             {
                 navn: "Negative tall: addisjon og subtraksjon",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["pluss", "minus", "subtraksjon", "addisjon", "negative", "tall"]
             },
             {
                 navn: "Negative tall: multiplikasjon og divisjon",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["ganging", "deling", "multiplikasjon", "divisjon", "negative", "tall"]
             },
             {
                 navn: "Algebra: addisjon og subtraksjon",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["pluss", "minus", "subtraksjon", "addisjon", "algebra"]
             },
             {
                 navn: "Algebra: multiplikasjon og divisjon",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["ganging", "deling", "multiplikasjon", "divisjon", "algebra"]
             },
             {
                 navn: "Algebra: ledd (VIKTIG)",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["algebra", "ledd", "viktig"]
             },
             {
                 navn: "Algebra: Den distributive lov (VIKTIG)",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["viktig", "algebra", "lov", "den", "distributive"]
             },
             {
                 navn: "Likninger: flytte/bytte og deling",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["likninger", "flytte/bytte", "dele", "deling"]
             },
             {
                 navn: "Likninger: brøk",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["likninger", "brøk"]
             },
             {
                 navn: "Faktorisere tall",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["faktorisering/faktorisere", "tall"]
             },
             {
                 navn: "Figurtall: begreper og forklaringer",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["mønster", "figurtall", "begreper", "forklaring/forklare/forklart"]
             },
             {
                 navn: "Figurtall: finne formel til mønster",
                 lettVideo: "",
                 dypereVideo: "",
+                oppgaver: "",
                 keywords: ["figurtall", "geometrisk/mønster", "formel"]
             }
         ]
@@ -127,7 +152,13 @@ const data = [
     {
         trinn: "Viktige konsepter",
         temaer: [
-
+            {
+                navn: "Føring - utregning",
+                lettVideo: "",
+                dypereVideo: "",
+                oppgaver: "",
+                keywords: ["føring", "utregning"]
+            }
         ]
     },
 ];
@@ -139,6 +170,7 @@ Tema blueprint:
     navn: "",
     lettVideo: "",
     dypereVideo: "",
+    oppgaver: "",
     keywords: [""]
 }
 
