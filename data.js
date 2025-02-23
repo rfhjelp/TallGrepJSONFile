c08c5c256894096d99416baf3dca0b2318fb9a35
rfhjelp <andblugje@gmail.com>
2025-02-23 11:38:55 +0100

    javascript not json

---
+++ data.js
@@ -0,0 +1,151 @@
+const data = [
+    {
+        trinn: "8. trinn",
+        temaer: [
+            {
+                navn: "De fire regneartene: addisjon og subtraksjon",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["pluss", "minus", "subtraksjon", "addisjon", "regning", "tall"]
+            },
+            {
+                navn: "De fire regneartene: multiplikasjon og divisjon",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["ganging", "deling", "multiplikasjon", "divisjon", "regning", "tall"]
+            },
+            {
+                navn: "Brøk: addisjon og subtraksjon",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["pluss", "minus", "subtraksjon", "addisjon", "brøk"]
+            },
+            {
+                navn: "Brøk: multiplikasjon og divisjon",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["ganging", "deling", "multiplikasjon", "divisjon", "brøk"]
+            },
+            {
+                navn: "Tall regning: potenser og paranteser",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["tall", "regning", "potens", "eksponent", "grunntall", "parantes"]
+            },
+            {
+                navn: "Tall regning: kvadratrot og kvadrattall",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["tall", "regning", "kvadratrot", "kvadrattall"]
+            },
+            {
+                navn: "Tall regning: PEMDAS",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["tall", "regning", "rekkefølgen", "pemdas", "regne"]
+            },
+            {
+                navn: "Negative tall: addisjon og subtraksjon",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["pluss", "minus", "subtraksjon", "addisjon", "negative", "tall"]
+            },
+            {
+                navn: "Negative tall: multiplikasjon og divisjon",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["ganging", "deling", "multiplikasjon", "divisjon", "negative", "tall"]
+            },
+            {
+                navn: "Algebra: addisjon og subtraksjon",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["pluss", "minus", "subtraksjon", "addisjon", "algebra"]
+            },
+            {
+                navn: "Algebra: multiplikasjon og divisjon",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["ganging", "deling", "multiplikasjon", "divisjon", "algebra"]
+            },
+            {
+                navn: "Algebra: ledd (VIKTIG)",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["algebra", "ledd", "viktig"]
+            },
+            {
+                navn: "Algebra: Den distributive lov (VIKTIG)",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["viktig", "algebra", "lov", "den", "distributive"]
+            },
+            {
+                navn: "Likninger: flytte/bytte og deling",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["likninger", "flytte/bytte", "dele", "deling"]
+            },
+            {
+                navn: "Likninger: brøk",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["likninger", "brøk"]
+            },
+            {
+                navn: "Faktorisere tall",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["faktorisering/faktorisere", "tall"]
+            },
+            {
+                navn: "Figurtall: begreper og forklaringer",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["mønster", "figurtall", "begreper", "forklaring/forklare/forklart"]
+            },
+            {
+                navn: "Figurtall: finne formel til mønster",
+                lettVideo: "",
+                dypereVideo: "",
+                keywords: ["figurtall", "geometrisk/mønster", "formel"]
+            }
+        ]
+    },
+    {
+        trinn: "9. trinn",
+        temaer: [
+
+        ]
+    },
+    {
+        trinn: "10. trinn",
+        temaer: [
+
+        ]
+    },
+    {
+        trinn: "Viktige konsepter",
+        temaer: [
+
+        ]
+    },
+];
+export default data;
+
+/*
+Tema blueprint:
+{
+    navn: "",
+    lettVideo: "",
+    dypereVideo: "",
+    keywords: [""]
+}
+
+
+trinn blueprint:
+{
+    trinn: "år. trinn",
+    temaer: [...]
+}
+*/
